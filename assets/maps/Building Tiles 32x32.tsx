7beff9a566d7c13be5b8ec79c2332f1d7465728c
Fernando <fer.ague.villa.02@gmail.com>
2026-02-01 19:53:23 -0300

    patrones para las texturas

---
+++ assets/maps/Building Tiles 32x32.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Building Tiles 32x32" tilewidth="32" tileheight="32" tilecount="264" columns="22">
+ <image source="../images/items/Building Tiles 32x32.png" width="704" height="384"/>
+ <tile id="15">
+  <objectgroup>
+   <object id="1" type="BloqueLadrillo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup>
+   <object id="1" type="BloqueLadrillo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup>
+   <object id="1" type="BloqueLadrillo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="BloqueLadrillo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
