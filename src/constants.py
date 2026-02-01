2a4f4905f5f6dea8248ad6a251c2e04e6aa331a1
wilhayla <wilhayla@hotmail.com>
2026-02-01 12:12:06 -0300

    agregando variables globales

--- src/constants.py
+++ src/constants.py
@@ -15,3 +15,14 @@ PLAYER_START_Y = 150
 LAYER_NAME_PLATFORMS = "Platforms"
 LAYER_NAME_OBSTACLES = "Obstacles"
 LAYER_NAME_ITEMS = "Items"
+
+# --- Lógica de Progresión ---
+POINTS_TO_LEVEL_UP = 500      # Cada cuántos puntos sube la dificultad
+LEVELS_TO_NEXT_WORLD = 3      # Cada cuántos niveles de dificultad cambiamos de bioma (Hielo -> Ciudad)
+INITIAL_GAME_SPEED = 5        # Velocidad inicial del suelo y obstáculos
+SPEED_INCREMENT = 0.5         # Cuánto aumenta la velocidad al subir de nivel
+
+# --- Identificadores de Mundos ---
+WORLD_ARCTIC = "Arctic"
+WORLD_CITY = "City"
+WORLD_OCEAN = "Ocean"
