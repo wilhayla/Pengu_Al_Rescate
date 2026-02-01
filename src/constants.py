f35d9ab695b6f6c304444986bf3e004652dd1e91
wilhayla <wilhayla@hotmail.com>
2026-02-01 03:30:26 -0300

    Agregando variables principales del juego

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+# Colores, tamaños de pantalla, gravedades,  etc
+
+# --- Configuración de Pantalla ---
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Pengu al Rescate: Misión Ambiental"
+
+# --- Físicas y Movimiento ---
+GRAVITY = 0.5
+PLAYER_JUMP_SPEED = 12
+PLAYER_START_X = 100
+PLAYER_START_Y = 150
+
+# --- Capas del Mapa (Para Tiled) ---
+LAYER_NAME_PLATFORMS = "Platforms"
+LAYER_NAME_OBSTACLES = "Obstacles"
+LAYER_NAME_ITEMS = "Items"
