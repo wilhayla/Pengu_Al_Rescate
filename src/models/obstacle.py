32bbc109f2fd3eb50c75889c672d5f799f7851c3
wilhayla <wilhayla@hotmail.com>
2026-02-02 14:21:42 -0300

    agregar parametro delta al metodo update

--- src/models/obstacle.py
+++ src/models/obstacle.py
@@ -11,11 +11,17 @@ class Obstacle(arcade.Sprite):
         self.center_x = SCREEN_WIDTH + 100
         self.center_y = 110 # Un poco arriba del suelo
         
-        self.speed = speed
+        # self.speed = speed
+        # En lugar de una variable 'speed' custom, usamos la de Arcade
+        self.change_x = -speed
     
-    def update(self):
+    def update(self, delta_time: float = 1 / 60):
         # Mover a la izquierda
-        self.center_x -= self.speed
+        # self.center_x -= self.speed
+        
+        # 1. Dejamos que Arcade mueva el sprite automáticamente 
+        # (Esto usa el valor de self.change_x que definiste en el __init__)
+        super().update()
         
         # Si sale de la pantalla, eliminarse
         if self.right < 0:
