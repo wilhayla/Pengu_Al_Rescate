3d32b7462444778de10737babbc8c20d7a79eaf6
wilhayla <wilhayla@hotmail.com>
2026-02-03 21:29:40 -0300

    archivo mapa

---
+++ Building Tiles 32x32.tsx
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Building Tiles 32x32" tilewidth="32" tileheight="32" tilecount="264" columns="22">
+ <image source="Building Tiles 32x32.png" width="704" height="384"/>
+ <tile id="15">
+  <objectgroup>
+   <object id="1" type="LadrilloSuelo" x="0" y="27" width="32" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup>
+   <object id="1" type="BloqueLadrillo" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="BloqueLadrillo" x="0" y="0" width="32" height="32"/>
+   <object id="2" type="LadrilloSuelo" x="0" y="27" width="32" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="81" type="Ladrillo">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="BloqueLadrillo" x="0" y="0" width="32" height="32"/>
+   <object id="2" type="Ladrillo" x="0" y="0" width="32" height="32"/>
+   <object id="3" name="Ladrillo" x="0" y="0" width="32" height="32"/>
+   <object id="4" x="0" y="0" width="32" height="32"/>
+   <object id="5" x="0" y="0" width="32" height="32"/>
+   <object id="6" type="sss" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
